fefa2856cd3fe6fba9573bc5806e3e21c60ec2de
C. Daniel Sanchez R <cdsanchezr@unal.edu.co>
2023-10-19 17:43:31 -0500

    S3 fixing.

--- src/app/imagesBucket/page.tsx
+++ src/app/imagesBucket/page.tsx
@@ -4,18 +4,22 @@ import ImageGallery from './gallery';
 async function loadImages() {
 
   const headers: HeadersInit = {
-      'Content-Type': 'application/json',
-      'x-api-key': process.env.AWS_S3_LAMBDA_APIKEY || '',
-    }
+    'Content-Type': 'application/json',
+    'x-api-key': process.env.AWS_S3_LAMBDA_APIKEY || '',
+  }
 
   const res = await fetch(process.env.AWS_S3_LAMBDA_URL || '', {
     method: 'GET',
     headers,
+    cache: 'no-store',
   });
   const { images }: { images: string[]} = await res.json();
 
   return { images };
 }
+
+export const fetchCache = 'force-no-store'
+
 
 const YourImagePage: React.FC = async () => {
   const {images} = await loadImages();
